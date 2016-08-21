f9ddba97326ab9d2a03bf62ff5952a421ecefc8d
Max <max.blum@gmail.com>
2016-08-21 09:38:47 -0400

    Add Auth form

--- client/views/auth.jsx
+++ client/views/auth.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
 export default function AuthView() {
-  return <h1>This is an Auth View</h1>
+  return (
+    <form
+      action='/endpoint'
+    >
+      <h1>This is an Auth View</h1>
+      <label htmlFor='username'>Username</label>
+      <input type='text' name='username'/>
+      <label htmlFor='password'>Password</label>
+      <input type='password' name='password'/>
+      <input type='submit'/>
+    </form>
+  )
 }
