bfb00deb71a8cc8d097914ce10e66f60e99a2928
Max <max.blum@gmail.com>
2016-08-20 20:31:43 -0400

    Add module resolution logic

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,8 +1,16 @@
 import path from 'path'
 
 export default {
+  resolve: {
+    root: [
+      path.resolve(__dirname, 'client'),
+    ],
+    extensions: ['', '.js', '.jsx', '.scss', '.json'],
+    modulesDirectories: ['node_modules'],
+    fallback: path.join(__dirname, 'node_modules'),
+  },
   devtool: '#eval-source-map',
-  entry: './client/client.jsx',
+  entry: 'client',
   output: {
     path: path.resolve(__dirname, 'build'),
     publicPath: '/',
