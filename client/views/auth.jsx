4b86c6b84f360747009e6953ca14b554965de672
Max <max.blum@gmail.com>
2016-08-23 21:12:06 -0400

    Add placeholder auth handler and state

--- client/views/auth.jsx
+++ client/views/auth.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 
-export default function AuthView() {
+import {post} from 'lib/api'
+
+export default function AuthView({typedUsername, typedPassword}) {
   return (
     <form
       action='/endpoint'
+      onSubmit={e => {
+        e.preventDefault()
+        post('/some-endpoint', {username: typedUsername, password: typedPassword})
+      }}
     >
       <h1>This is an Auth View</h1>
       <label htmlFor='username'>Username</label>
-      <input type='text' name='username'/>
+      <input type='text' name='username' value={typedUsername}/>
       <label htmlFor='password'>Password</label>
-      <input type='password' name='password'/>
+      <input type='password' name='password' value={typedPassword}/>
       <input type='submit'/>
     </form>
   )
+}
+
+AuthView.propTypes = {
+  typedUsername: React.PropTypes.string,
+  typedPassword: React.PropTypes.string,
+}
+
+AuthView.defaultProps = {
+  typedUsername: '',
+  typedPassword: '',
 }
