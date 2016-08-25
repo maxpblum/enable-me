952e480024f7aa22d3bfe723f76effd5cf9d72cf
Max <max.blum@gmail.com>
2016-08-25 16:32:55 -0400

    Add secure password storage

--- server/server.babel.js
+++ server/server.babel.js
@@ -1,5 +1,11 @@
 import express from 'express'
 import logging from 'winston'
+import bodyParser from 'body-parser'
+import bcrypt from 'bcrypt'
+
+import {User} from './models'
+
+const Promise = global.Promise
 
 const app = express()
 
@@ -11,9 +17,24 @@ function getBundleLocation() {
 const indexPage = (req, res) => res.render('index', {bundleLocation: getBundleLocation()})
 
 app.set('view engine', 'pug')
+app.use(bodyParser.json())
 app.use('/build', express.static('build'))
 app.get('/', indexPage)
-app.post('/some-endpoint', (req, res) => res.send('it worked!'))
+app.post('/users', (req, res) =>
+  bcrypt.genSalt(10, (err, salt) =>
+    bcrypt.hash(req.body.password, salt, (err, hash) =>
+      new Promise((resolve) => {
+        if (err) { throw err }
+        resolve(User.create({
+          name: req.body.username,
+          passwordSalt: salt,
+          hashedPassword: hash,
+        }))
+      }).then(user => res.status(200).send(user))
+      .catch(err => res.status(400).send(err))
+    )
+  )
+)
 
 const port = process.env.PORT || 3000
 logging.info(`Listening on port ${port}`)
