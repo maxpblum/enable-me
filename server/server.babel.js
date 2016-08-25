df18441643f325a35b14ddd155f1acd9177893c6
Max <max.blum@gmail.com>
2016-08-25 19:22:48 -0400

    Move body and cookie parsing into main server file

--- server/server.babel.js
+++ server/server.babel.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 import logging from 'winston'
 
-import attachEndpoints from './handlers/endpoints'
+import attachHandlers from './handlers/main'
 
 const app = express()
-attachEndpoints(app)
+app.use(cookieParser())
+app.use(bodyParser.json())
+attachHandlers(app)
 
 const port = process.env.PORT || 3000
 logging.info(`Listening on port ${port}`)
