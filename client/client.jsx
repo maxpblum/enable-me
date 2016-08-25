bf4a788b4e22fd65dd51d2f418b761f5aa62a811
Max <max.blum@gmail.com>
2016-08-24 20:57:00 -0400

    Pass the store to the Authview

--- client/client.jsx
+++ client/client.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 
 import AuthView from 'views/auth'
 
+import store from 'store'
+
 ReactDOM.render(
-  <AuthView/>,
+  <AuthView store={store}/>,
   document.getElementById('root')
 )
