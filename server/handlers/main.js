365d8355bfbcb5a3b4c09a7b706fcac596be4f37
Max <max.blum@gmail.com>
2016-08-25 19:21:48 -0400

    Rename endpoints.js to handlers/main

--- server/handlers/main.js
+++ server/handlers/main.js
@@ -1,12 +1,11 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 
-import {signUp} from './authHandlers'
+import {signUp, getSessionUser} from './authHandlers'
 import {indexPage} from './pageHandlers'
 
 export default function attachEndpoints(app) {
   app.set('view engine', 'pug')
-  app.use(bodyParser.json())
+  app.use(getSessionUser)
   app.use('/build', express.static('build'))
   app.get('/', indexPage)
   app.post('/users', signUp)
