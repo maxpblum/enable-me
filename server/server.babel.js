d861f33c6d40c31a8a0c295da3c41ceac110b048
Max Blum <max@tictail.com>
2016-09-03 17:06:08 -0400

    Add SSL support

--- server/server.babel.js
+++ server/server.babel.js
@@ -1,3 +1,6 @@
+import https from 'https'
+import fs from 'fs'
+import http from 'http'
 import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
@@ -12,4 +15,11 @@ attachHandlers(app)
 
 const port = process.env.PORT || 3000
 logging.info(`Listening on port ${port}`)
-app.listen(process.env.PORT || 3000)
+
+if (process.env.SSL_KEY && process.env.SSL_CERT) {
+  const pkey = fs.readFileSync('./key.pem')
+  const pcert = fs.readFileSync('./cert.pem')
+  https.createServer({key: pkey, cert: pcert}, app).listen(port)
+} else {
+  http.createServer(app).listen(port)
+}
