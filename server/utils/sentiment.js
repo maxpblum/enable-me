9e7e95a1c483c9862434ccd7fb11eaf0d87dab22
Max Blum <max@tictail.com>
2016-09-03 11:37:35 -0400

    Update sentiment utility to return first negative sentence

--- server/utils/sentiment.js
+++ server/utils/sentiment.js
@@ -4,10 +4,12 @@ import shellEscape from 'shell-escape'
 const PYTHON_PATH = 'server/get_polarity.py'
 const PYTHON_BIN = process.env.PYTHON
 
-export default function(text) {
+export default function firstBadSentence(text) {
   const escaped = shellEscape([text])
   const output = childProcess.execSync(
     `${PYTHON_BIN} ${PYTHON_PATH} ${escaped}`
   ).toString('utf-8')
-  return JSON.parse(output)
+  const sentences = JSON.parse(output)
+  const badSentence = sentences.find(([, score]) => score <= 0)
+  return badSentence && badSentence[0]
 }
