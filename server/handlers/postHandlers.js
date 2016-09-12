8b590e7de447a8c597ae973b8ade9f47d67bdc5d
Max Blum <max@tictail.com>
2016-09-12 18:56:29 +0000

    Create post and associate with user in one transaction

--- server/handlers/postHandlers.js
+++ server/handlers/postHandlers.js
@@ -8,8 +8,7 @@ import firstBadSentence from '../utils/sentiment'
 export function createPost(req, res) {
   getLoggedInUser(req, req.params.userId)
   .then(user =>
-    Post.create({text: req.body.post})
-    .then(post => post.setUser(user))
+    Post.create({text: req.body.post, user_id: user.id})
   )
   .then(() => res.sendStatus(200))
   .catch(handleUnauthorized(res))
